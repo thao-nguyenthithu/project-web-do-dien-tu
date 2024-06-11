863c36316edb21cd40ca38a44fc9599ecb29e649
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2024-06-11 13:50:10 +0700

    add about item

---
+++ assets/js/home.js
@@ -0,0 +1,32 @@
+function getHomePage() {
+    return `
+        <!-- Home Main -->
+            <div class="home__top">
+                <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/06/banner/Banner-big---Desk-1920x450-1.jpg" alt="">
+            </div>
+            <!-- slide adv -->
+            <div class="gird">
+                <div class="slideshow-container">
+                    <div class="mySlides slide">
+                        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/06/banner/720-220-720x220-9-720x220-1.png" style="width:100%">
+                    </div>
+                    <div class="mySlides slide">
+                        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/06/banner/banner-may-lanh-chung-720x220-2.jpg" style="width:100%">
+                    </div>
+                    <div class="mySlides slide">
+                        <img src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/06/banner/Ro1-min-720x220.png" style="width:100%">
+                    </div>
+
+                     <!-- Next and previous buttons -->
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                </div>
+                <br>
+                <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span> 
+                    <span class="dot" onclick="currentSlide(2)"></span> 
+                    <span class="dot" onclick="currentSlide(3)"></span> 
+                </div>
+            </div>
+    `;
+}
---
+++ assets/js/styles.js
@@ -0,0 +1,44 @@
+function initSilde(){
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.ab-slide__adv');
+    const slidesContainer = document.querySelector('.ab-slides');
+    let slideInterval;
+
+    function showSlide(index) {
+        slidesContainer.style.transform = `translateX(${-index * 50}%)`; // 50% to move by two slides at a time
+    }
+
+    function moveSlide(n) {
+        currentIndex += n;
+        if (currentIndex >= slides.length - 1) {
+            currentIndex = 0;
+        } else if (currentIndex < 0) {
+            currentIndex = slides.length - 2;
+        }
+        showSlide(currentIndex);
+    }
+
+    function currentSlide(index) {
+        currentIndex = index;
+        showSlide(currentIndex);
+    }
+
+    function startAutoSlide() {
+        slideInterval = setInterval(() => {
+            moveSlide(1);
+        }, 3000); // Change slide every 3 seconds
+    }
+
+    function stopAutoSlide() {
+        clearInterval(slideInterval);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        showSlide(currentIndex);
+        startAutoSlide();
+    });
+
+    document.querySelector('.about-slider').addEventListener('mouseover', stopAutoSlide);
+    document.querySelector('.about-slider').addEventListener('mouseout', startAutoSlide);
+
+}
